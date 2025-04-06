ce6c46ea079c34cfbfd025ba1d66e73a1aeb351e
Elizabeth <evonbergen1@live.maryville.edu>
2025-04-06 15:43:26 -0500

    add basic homepage content

--- capstone/src/main.js
+++ capstone/src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './Homepage.vue'
 import * as VueRouter from 'vue-router'
+
+//import pages below
 import TradePage from './pages/TradePage.vue'
 import ClothingPage from './pages/ClothingPage.vue'
 import ClothingDetailPage from './pages/ClothingDetailPage.vue'
+//import LoginPage from './pages/LoginPage.vue'
+//import SignupPage from './pages/SignupPage.vue'
 
 //routing for different views
 createApp(App)
@@ -18,6 +22,13 @@ createApp(App)
     }, {
         path: '/clothing/:clothingId',
         component: ClothingDetailPage,
-    }]
+    }, //{                                  //commented out for simplified future development
+        //path: '/signup',
+        //component: SignupPage,
+    //}, { 
+        //path: '/login'
+        //component: LoginPage,
+    //},
+    ]
 }))
 .mount('#app')
