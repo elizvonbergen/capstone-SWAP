b33228b322d46051e43a38bc6f0ef7347f1c533c
Elizabeth <evonbergen1@live.maryville.edu>
2025-04-06 16:54:43 -0500

    create signup form

--- capstone/src/pages/SignupPage.vue
+++ capstone/src/pages/SignupPage.vue
@@ -1,11 +1,28 @@
 <template>
     <h1>Sign Up</h1>
+    <form @submit.prevent="signup">
+      <input v-model="email" placeholder="Email" />
+      <input v-model="password" type="password" placeholder="Password">
+      <button type="submit">Sign Up</button>
+    </form>
 </template>
 
-<script>
-    export default {
-  name: 'SignupPage',
-  components: {
+<script setup>
+import { ref } from 'vue'
+import { auth } from '../firebase/firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { useRouter } from 'vue-router'
+
+const email = ref('')
+const password = ref('')
+const router = useRouter()
+
+const signup = async () => {
+  try {
+    await createUserWithEmailAndPassword(auth, email.value, password.value)
+    router.push('/profile') // redirect after signup
+  } catch (err) {
+    console.error(err)
   }
 }
 </script>
