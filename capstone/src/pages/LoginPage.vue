7d1bef1f77cb96359ce20710f30cc2bb6769fb70
Elizabeth <evonbergen1@live.maryville.edu>
2025-04-06 21:55:10 -0500

    login functional

--- capstone/src/pages/LoginPage.vue
+++ capstone/src/pages/LoginPage.vue
@@ -1,11 +1,33 @@
 <template>
     <h1>Log In</h1>
+    <form @submit.prevent="login">
+      <input v-model="email" placeholder="Email" />
+      <input v-model="password" type="password" placeholder="Password">
+      <button type="submit">Log In</button>
+      <p v-if="error">{{ error }}</p>
+    </form>
 </template>
 
-<script>
-    export default {
-  name: 'LoginPage',
-  components: {
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { auth } from '../firebase/firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+
+const email = ref('')
+const password = ref('')
+const error = ref('')
+const router = useRouter()
+
+const login = async () => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email.value, password.value)
+    const user = userCredential.user
+    if (user && user.uid) {
+      router.push(`/profile/${user.uid}`)
+    }
+  } catch (err) {
+    console.error(err)
   }
 }
 </script>
