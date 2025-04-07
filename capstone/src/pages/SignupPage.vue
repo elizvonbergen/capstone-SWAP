526d87c404b418268c4f06213131795010072642
Elizabeth <evonbergen1@live.maryville.edu>
2025-04-07 11:37:39 -0500

    working database

--- capstone/src/pages/SignupPage.vue
+++ capstone/src/pages/SignupPage.vue
@@ -1,6 +1,8 @@
 <template>
     <h1>Sign Up</h1>
     <form @submit.prevent="signup">
+      <input v-model="name" placeholder="First Name" />
+      <input v-model="username" placeholder="Username" />
       <input v-model="email" placeholder="Email" />
       <input v-model="password" type="password" placeholder="Password">
       <button type="submit">Sign Up</button>
@@ -11,9 +13,12 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { auth } from '../firebase/firebase'
+import { auth, db } from '../firebase/firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
 
+const name = ref('')
+const username = ref('')
 const email = ref('')
 const password = ref('')
 const error = ref('')
@@ -23,8 +28,17 @@ const signup = async () => {
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value)
     const user = userCredential.user
+
+    //save user info in firestore
+    await setDoc(doc(db, 'users', user.uid), {
+      name: name.value,
+      username: username.value,
+      email: email.value,
+      createdAt: new Date(),
+    })
+
     if (user && user.uid) {
-      router.push(`/profile/${user.uid}`)
+      router.push(`/profile/${user.uid}`) //push to own profile after login
     }
   } catch (err) {
     console.error(err)
