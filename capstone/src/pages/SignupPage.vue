3e9151e2af9fe4267e8f72649b3cfcab4b24c08e
Elizabeth <evonbergen1@live.maryville.edu>
2025-04-06 20:44:10 -0500

    user auth working

--- capstone/src/pages/SignupPage.vue
+++ capstone/src/pages/SignupPage.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>Sign Up</h1>
-    <form @submit="signup">
+    <form @submit.prevent="signup">
       <input v-model="email" placeholder="Email" />
       <input v-model="password" type="password" placeholder="Password">
       <button type="submit">Sign Up</button>
@@ -10,16 +10,22 @@
 
 <script setup>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { auth } from '../firebase/firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 const email = ref('')
 const password = ref('')
 const error = ref('')
+const router = useRouter()
 
 const signup = async () => {
   try {
-    await createUserWithEmailAndPassword(auth, email.value, password.value)
+    const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value)
+    const user = userCredential.user
+    if (user && user.uid) {
+      router.push(`/profile/${user.uid}`)
+    }
   } catch (err) {
     console.error(err)
   }
