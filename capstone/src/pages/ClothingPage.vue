b5350c8cae8d176859bf9a117afb4e4b248f0ba5
Elizabeth <evonbergen1@live.maryville.edu>
2025-04-09 23:25:49 -0500

    add search feature

--- capstone/src/pages/ClothingPage.vue
+++ capstone/src/pages/ClothingPage.vue
@@ -1,8 +1,9 @@
 <template>
     <section>
       <h1>Current Listings</h1>
+      <input v-model="searchQuery" placeholder="Search by name..." /> <!-- search function -->
       <ul>
-        <li v-for="item in userListings" :key="item.id">
+        <li v-for="item in searchListings" :key="item.id">
           <p><RouterLink :to="`/listings/${item.id}`">{{ item.name }}</RouterLink></p>
           <p><RouterLink :to="`/profile/${item.ownerId}`">{{ item.ownerUsername }}</RouterLink></p>
           <p>{{ item.description }}</p>
@@ -13,11 +14,17 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { db } from '../firebase/firebase'
 import { collection, getDocs, query, orderBy, doc, getDoc } from 'firebase/firestore'
 
 const userListings = ref([])
+const searchQuery = ref('')
+
+const searchListings = computed(() => { //computes listings to show based on user search input
+  return userListings.value.filter((item) =>
+    item.name.toLowerCase().includes(searchQuery.value.toLowerCase()))
+})
 
 onMounted(async () => {
     //get all listings
